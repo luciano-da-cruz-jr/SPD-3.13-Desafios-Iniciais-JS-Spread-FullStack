c6ade1c31e3abe02ec7c1a6c74cf5cb15534a7a9
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-08-23 08:02:45 -0300

    JavaScript Terminal Console (Node) - Imprimir a palavra Ho de acordo com a quantidade informada V1

---
+++ 03 - JavaScript Terminal Console (Node) - Imprimir a palavra Ho de acordo com a quantidade informada/script.js
@@ -0,0 +1,15 @@
+let frase = "HO";
+let N = 5;
+
+for(let i=1; i<=N; i++)
+{
+    if(i === N)
+    {
+       frase += "!";   
+    }else
+    {
+        frase += " " + "HO";
+    }
+}
+
+console.log(frase);
